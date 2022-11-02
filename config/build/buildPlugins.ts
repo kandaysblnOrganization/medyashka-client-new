de3f7f812165f1f83c87ba8465d2e895d5e41a32
Berador Stambekov <stambekovbera@gmail.com>
2022-11-03 03:52:53 +0600

    initialize project & config webpack

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import {
+    BuildOptions
+} from "./types/config";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export const buildPlugins = ({paths}: BuildOptions): webpack.WebpackPluginInstance[] => {
+    return [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        })
+    ];
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Counter} from "./components/Counter/Counter";
+import './assets/scss/reset.scss';
+
+const App = () => {
+    return (
+        <div>
+            <Counter/>
+        </div>
+    );
+};
+
+export default App;
